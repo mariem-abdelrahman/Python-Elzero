87ac7c3cd84bc6910b8a5fcf228eb9292d56e9a8
mariem-abdelrahman <mariemabdelrahmanmohammed@gmaile.com>
2026-08-30 21:40:28 +0300

    Add examples for Python type conversion, including set and dictionary usage

--- LESSON37/examples37.py
+++ LESSON37/examples37.py
@@ -28,3 +28,29 @@ print(list(c))
 print(list(d))
 print(list(e))
 print(list(f))
+
+# set()
+
+c = "mariem" # string
+d = (1 , 2 , 3 , 4 , 5) # tuple
+e = ["a" , "b" , "c"] # list
+f = {"a" : 1 , "b" : 2} # dictionary
+
+print(set(c))
+print(set(d))
+print(set(e))
+print(set(f))
+
+# dict()
+
+# c = "mariem" # string # error there is no key & value 
+# d1 = (1 , 2 , 3 , 4 , 5) # tuple # error 
+d = (("a" , 1) , ("b" , 2) , ("c" , 3)) # nested tuple is ok 
+# e1 = ["a" , "b" , "c"] # list # error 
+e = [["one" , 1] , ["two" , 2] , ["three" , 3]] # list
+# f = {"a" : 1 , "b" : 2} # set # error # un hashable type
+
+# print(dict(c))
+print(dict(d))
+print(dict(e))
+# print(dict(f))
